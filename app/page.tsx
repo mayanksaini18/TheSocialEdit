fba16df77dffc419208bc3f3c005f1b9cd8f4db9
Mayank Saini <mayanksaini0416@gmail.com>
2026-01-14 10:28:30 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,28 @@
-import Clients from "@/app/components/Clients";
-
-import ContactSection from "@/app/components/ContactSection";
 import Container from "@/app/components/Container";
 import FadeIn from "@/app/components/FadeIn";
-import Services from "@/app/components/Services";
-import Testimonials from "@/app/components/Testimonials";
-import ScrollCard from "@/app/components/ScrollCard"
-import InnovationSection from "./components/InnovationSection";
+import ScrollCard from "@/app/components/ScrollCard";
+import InnovationSection from "@/app/components/InnovationSection";
 
 export default function Home() {
   return (
-    <main className=" text-black">
+    <main className="text-neutral-100">
       <Container className="mt-24 sm:mt-32">
         <FadeIn className="max-w-3xl">
-          <h1 className="font-display text-5xl font-medium tracking-tight text-black text-balance sm:text-7xl">
+          <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-600 text-balance sm:text-7xl">
             Elevating Brands with{" "}
             <span className="text-yellow-500">Content</span>,{" "}
             <span className="text-yellow-500">Strategy</span> and{" "}
             <span className="text-yellow-500">Web Solutions</span>.
           </h1>
- <p className="mt-6 text-xl text-neutral-300">
+ <p className="mt-6 text-xl text-neutral-500">
             We are a content marketing agency that partners with brands to
             deliver powerful brand strategy and bespoke web solutions, building
             an online presence that drives growth and engagement.
           </p> 
         </FadeIn>
       </Container>
-
-<InnovationSection/>
+      <ScrollCard />
+      <InnovationSection />
     </main>
   );
 }
-
