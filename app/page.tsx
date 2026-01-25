c8eae4cc61c0eac7e55cee80ecc46c0be9719db6
Mayank Saini <mayanksaini0416@gmail.com>
2026-01-25 13:13:34 +0530

    work section added

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,15 @@ import ScrollCard from "@/app/components/ScrollCard";
 import InnovationSection from "@/app/components/InnovationSection";
 import HeroSection from "./components/HeroSection";
 import FaqSection from "./components/FaqSection";
+import WorkSection from "./components/WorkSection";
 export default function Home() {
   return (
     <main className="text-neutral-100">
           <HeroSection/>
       
           <ScrollCard />
-    
       <InnovationSection />
+      <WorkSection/>
       <FaqSection/>
     </main>
   );
